e9359ffd9cc5a57d2f367244009aed91ae6485e2
Dishant Vyas <dishant.vyas@eynos.com>
2020-03-11 23:33:36 +0530

    Added Files

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,7 @@ class Stock < ApplicationRecord
   validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+    client = IEX::Api::Client.new('Tpk_40713aafc2204fb19fb1aba31640604d',
                                   endpoint: 'https://sandbox.iexapis.com/v1')
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
